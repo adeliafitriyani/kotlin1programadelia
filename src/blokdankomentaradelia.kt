2814cb349a22a1d0725d1201d396948543ed929e
adeliafitriyani <adeliafitriani9@gmail.com>
2025-09-08 20:14:28 +0700

    3 Blok & Komentar

---
+++ src/blokdankomentaradelia.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    println ("hello, program pertama di kotlin")
+    // Contoh momentar 1 baris
+    /*
+    Contoh Komentar multi baris
+     */
+    /**
+     * Kotlin Doc --> baris ini untuk mendokumentasikan kode program
+     */
+}
