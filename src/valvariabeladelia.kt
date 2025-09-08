d17c82dc1203fb53e096e6c3b97f5db339b1fdca
adeliafitriyani <adeliafitriani9@gmail.com>
2025-09-08 21:19:34 +0700

    6 val Variabel

---
+++ src/valvariabeladelia.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    var varVariabel : Int
+    varVariabel = 56 //assigning first value to varVariabel
+    println("Pemeberian nilai pertama pada vaVariabel = " +varVariabel)
+    varVariabel = 78 //assigning first value to varVariabel
+    println("Pemeberian nilai kedua pada vaVariabel = " +varVariabel)
+    val phi = 3.14
+    phi - 2.5
+
+
+}
