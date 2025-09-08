b3ff711fbe9de48ecc2410e8c8136f9a5358abc2
adeliafitriyani <adeliafitriani9@gmail.com>
2025-09-08 20:51:13 +0700

    5 Var Variabel

---
+++ src/varvariabeladelia.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    var varVariabel : Int
+    varVariabel = 56 //assigning first value to varVariabel
+    println("Pemeberian nilai pertama pada vaVariabel = " +varVariabel)
+    varVariabel = 78 //assigning first value to varVariabel
+    println("Pemeberian nilai kedua pada vaVariabel = " +varVariabel)
+
+}
